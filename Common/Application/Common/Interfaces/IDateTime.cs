6d50a62fc0b4ea4de2d1c2405d91eaa36fd6fc57
ilker Aytı <ilkerayti@gmail.com>
2020-08-10 21:46:12 +0300

    Add project files.

---
+++ Common/Application/Common/Interfaces/IDateTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interfaces
+{
+    public interface IDateTime
+    {
+        DateTime Now { get; }
+    }
+}
---
+++ Common/Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Services
+{
+    using System;
+
+    using Application.Common.Interfaces;
+
+    public class DateTimeService : IDateTime
+    {
+        public DateTime Now => DateTime.Now;
+    }
+}
