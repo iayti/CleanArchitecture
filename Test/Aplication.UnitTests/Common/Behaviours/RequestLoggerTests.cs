25d9e837bd356083fd565bc78afb95bf5704a9fc
ilker Aytı <ilkerayti@gmail.com>
2020-09-01 20:24:26 +0300

    1) RequestLoggerTests added. 2) MappingTests added.

---
+++ Test/Aplication.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Aplication.UnitTests.Common.Behaviours
+{
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Logging;
+
+    using Moq;
+    using Xunit;
+
+    using Application.Cities.Commands.Create;
+    using Application.Common.Behaviours;
+    using Application.Common.Interfaces;
+
+
+    public class RequestLoggerTests
+    {
+        private readonly Mock<ILogger<CreateCityCommand>> _logger;
+        private readonly Mock<ICurrentUserService> _currentUserService;
+        private readonly Mock<IIdentityService> _identityService;
+
+
+        public RequestLoggerTests()
+        {
+            _logger = new Mock<ILogger<CreateCityCommand>>();
+
+            _currentUserService = new Mock<ICurrentUserService>();
+
+            _identityService = new Mock<IIdentityService>();
+        }
+
+        [Fact]
+        public async Task ShouldCallGetUserNameAsyncOnceIfAuthenticated()
+        {
+            _currentUserService.Setup(x => x.UserId).Returns("Administrator");
+
+            var requestLogger = new LoggingBehaviour<CreateCityCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+            await requestLogger.Process(new CreateCityCommand { Name = "Bursa" }, new CancellationToken());
+
+            _identityService.Verify(i => i.GetUserNameAsync(It.IsAny<string>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ShouldNotCallGetUserNameAsyncOnceIfUnauthenticated()
+        {
+            var requestLogger = new LoggingBehaviour<CreateCityCommand>(_logger.Object, _currentUserService.Object, _identityService.Object);
+
+            await requestLogger.Process(new CreateCityCommand { Name = "Bursa" }, new CancellationToken());
+
+            _identityService.Verify(i => i.GetUserNameAsync(null), Times.Never);
+        }
+    }
+}
---
+++ Test/Aplication.UnitTests/Common/Mappings/MappingTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Aplication.UnitTests.Common.Mappings
+{
+    using System;
+
+    using MapsterMapper;
+    using Xunit;
+    using Moq;
+
+    using Application.Dto;
+    using Domain.Entities;
+    using FluentAssertions;
+    using Mapster;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class MappingTests
+    {
+        private readonly IMapper _mapper;
+
+        public MappingTests()
+        {
+            TypeAdapterConfig typeAdapterConfig = new TypeAdapterConfig();
+
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton(typeAdapterConfig);
+            services.AddScoped<IMapper, ServiceMapper>();
+
+            var sp = services.BuildServiceProvider();
+
+            using var scope = sp.CreateScope();
+            _mapper = scope.ServiceProvider.GetService<IMapper>();
+        }
+
+
+        [Theory]
+        [InlineData(typeof(City), typeof(CityDto))]
+        [InlineData(typeof(District), typeof(DistrictDto))]
+        public void ShouldSupportMappingFromSourceToDestination(Type source, Type destination)
+        {
+            var instance = Activator.CreateInstance(source);
+
+            _mapper.Map(instance, source, destination);
+        }
+
+        [Fact]
+        public void ShouldMappingCorrectly()
+        {
+            var city = new City { Id = 1, Name = "Bursa" };
+            var cityDto = _mapper.Map<City, CityDto>(city);
+            cityDto.Name.Should().Be("Bursa");
+        }
+    }
+}
