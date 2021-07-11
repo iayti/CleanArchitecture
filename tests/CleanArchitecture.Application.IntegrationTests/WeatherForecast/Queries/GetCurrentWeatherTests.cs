2d7a19162fcb303385c70d42ea163abc9e1e30ce
ilker Aytı <ilkerayti@gmail.com>
2021-07-11 14:54:35 +0300

    Removed OpenWeatherIntegrationTest (free token usage ended)

--- tests/CleanArchitecture.Application.IntegrationTests/WeatherForecast/Queries/GetCurrentWeatherTests.cs
+++ tests/CleanArchitecture.Application.IntegrationTests/WeatherForecast/Queries/GetCurrentWeatherTests.cs
@@ -8,22 +8,23 @@ namespace CleanArchitecture.Application.IntegrationTests.WeatherForecast.Queries
 {
     public class GetCurrentWeatherTests : TestBase
     {
-        [Test]
-        public async Task ShouldReturnCurrentWeather()
-        {
-            var query = new GetCurrentWeatherForecastQuery
-            {
-                Id = 2172797,
-                Lat = 1,
-                Lon = 1,
-                Q = "London%2Cuk"
-            };
-
-            var result = await SendAsync(query);
-
-            result.Should().NotBeNull();
-            result.Succeeded.Should().BeTrue();
-            result.Data.weather.Count.Should().BeGreaterThan(0);
-        }
+        //Removed bacuse of free token usage ended.!
+        // [Test]
+        // public async Task ShouldReturnCurrentWeather()
+        // {
+        //     var query = new GetCurrentWeatherForecastQuery
+        //     {
+        //         Id = 2172797,
+        //         Lat = 1,
+        //         Lon = 1,
+        //         Q = "London%2Cuk"
+        //     };
+        //
+        //     var result = await SendAsync(query);
+        //
+        //     result.Should().NotBeNull();
+        //     result.Succeeded.Should().BeTrue();
+        //     result.Data.weather.Count.Should().BeGreaterThan(0);
+        // }
     }
 }
